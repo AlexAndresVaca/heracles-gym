e194b76853f4a07cab8491356c41719c970f7e9b
allisonvanessa97 <allisonvanessa97@gmail.com>
2020-12-27 10:44:19 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\PagesController;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web HOLAAAAAAAA
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
