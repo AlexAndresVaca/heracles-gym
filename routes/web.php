af636bdb064692bd14078bc8713ec6b0f37457ef
AlexAxl80 <alex.vaca6676@utc.edu.ec>
2020-12-27 12:50:04 -0500

    clientes

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Carbon\Carbon;
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\ClientesController;
 
 /*
 |--------------------------------------------------------------------------
-| Web HOLAAAAAAAA
+| Web 
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
@@ -16,3 +17,9 @@ use App\Http\Controllers\PagesController;
 */
 
 Route::get('/', [PagesController::class, 'index'])->name('index');
+// RUTAS CRUD CLIENTES
+Route::get('inicio/clientes',[ClientesController::class, 'clientes_index'])->name('clientes.index');
+Route::POST('inicio/clientes/',[ClientesController::class,'clientes_add'])->name('clientes.add');
+Route::get('inicio/clientes/perfil/{id}',[ClientesController::class, 'clientes_perfil'])->name('clientes.perfil');
+Route::PUT('inicio/clientes/perfil/{id}/',[ClientesController::class, 'clientes_update'])->name('clientes.update');
+Route::POST('inicio/clientes/perfil/{id}/delete',[ClientesController::class, 'clientes_delete'])->name('clientes.delete');
