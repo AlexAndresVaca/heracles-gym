8541d5def7da1996078146775215fe6c57788009
AlexAxl80 <alex.vaca6676@utc.edu.ec>
2020-12-27 11:53:01 -0500

    ModeloCliente

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInite94ddcaa33a1f920387bc5b6d93d1f58
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInite94ddcaa33a1f920387bc5b6d93d1f58', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInite94ddcaa33a1f920387bc5b6d93d1f58', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInite94ddcaa33a1f920387bc5b6d93d1f58::getInitializer($loader));
         } else {
